093da469d2efef013d3fe22797820afe3bef7727
gilciaf <gilvan@ciaf.com.br>
2023-02-27 16:01:36 -0300

    primeiros passos code

--- src/App.vue
+++ src/App.vue
@@ -2,38 +2,46 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
-      dark
+      color="red"      
     >
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="./assets/embraseg.png"
           transition="scale-transition"
-          width="40"
+          width="140"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+     
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="#"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">Home</span>
+        <!-- <v-icon>mdi-open-in-new</v-icon> -->
+      </v-btn>
+      <v-btn
+        href="#"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Produtos</span>
+        <!-- <v-icon>mdi-open-in-new</v-icon> -->
+      </v-btn>
+      <v-btn
+        href="#"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Contato</span>
+        <!-- <v-icon>mdi-open-in-new</v-icon> -->
       </v-btn>
     </v-app-bar>
 
